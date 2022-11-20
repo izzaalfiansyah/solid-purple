42918deb8266b25aaa43ffe162ee793401c28ccc
izzaalfiansyah <iansyah724@gmail.com>
2022-11-20 20:15:54 +0700

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,7 @@
+export default () => {
+	return (
+		<div class="min-h-screen bg-gray-50 flex items-center justify-center text-white">
+			<div class="rounded-xl shadow p-4 w-[400px] h-[200px] bg-white">Hello World</div>
+		</div>
+	);
+};
